37f1d36935a4a83ea7c6dcebc50546df24eac62c
Giovanni Fusco <giofusco@gmail.com>
2019-10-08 16:22:28 -0700

    Initial Commit

---
+++ Unet_Detector/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Unet_Detector
+//
+//  Created by Giovanni Fusco on 10/8/19.
+//  Copyright © 2019 Smith-Kettlewell Eye Research Institute. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
