b950eae01a312a7d8e140fb0b70439de65e377b6
Giovanni Fusco <giofusco@gmail.com>
2020-01-24 13:18:49 -0800

    first commit

---
+++ Unet_Detector/Detector.swift
@@ -0,0 +1,9 @@
+//
+//  Detector.swift
+//  Unet_Detector
+//
+//  Created by Giovanni Fusco on 10/8/19.
+//  Copyright © 2019 Smith-Kettlewell Eye Research Institute. All rights reserved.
+//
+
+import Foundation
